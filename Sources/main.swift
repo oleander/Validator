060c08afe5038ca58855bd415fab916570ef5179
Linus Oleander <linus@oleander.nu>
2017-12-28 00:58:29 +0300

    ok

--- Sources/main.swift
+++ Sources/main.swift
@@ -33,7 +33,16 @@ extension Int: Validatorable {
 }
 
 struct Parameter<T: Validatorable> {
-  let checks: [T.Restriction]
+  let checks: [T.Restriction] = []
+  let options: [T] = []
+
+  init(checks: [T.Restriction]) {
+    self.checks = checks
+  }
+
+  init(options: [T]) {
+    self.options = options
+  }
 
   func parse(_ value: String) throws -> T {
     guard let result = T.parse(value) else {
@@ -53,5 +62,31 @@ let check = Parameter<Int>(
   checks: [.min(10), .max(90)]
 )
 
+enum Car: Validatorable, Restrictionable {
+  typealias Restriction = Self
+  case volvo, saab
+
+  func check(_ value: Car) throws {}
+
+  static func parse(_ value: String) -> Restriction? {
+    switch value {
+    case "volvo":
+      return .volvo
+    case "saab":
+      return .saab
+    default:
+      return nil
+    }
+  }
+}
+
+let opt = Parameter<Car>(
+  // flag: "max-age"  jjjj
+  // checks: [.min(10), .max(90)]
+  options: [.volvo]
+)
+
 assert((try? check.parse("11")) == 11)
 assert((try? check.parse("100")) == nil)
+assert((try? opt.parse("volvo")) == .volvo)
+assert((try? opt.parse("nothing")) == nil)
